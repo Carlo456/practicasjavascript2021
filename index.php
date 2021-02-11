5383335cd53302978c6e41d293801628384b00e7
CarloValenzuelaZepeda <gusvalenzuela77@gmail.com>
2021-02-11 16:40:54 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -6,16 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Created by Carlo Valenzuela">
     <title>Practica getelement</title>
 </head>
 
 <body>
     <p>Universidad Tecnológica de Durango</p>
-    <p>Nombre del alumno:</p>
-    <span>Carrera:</span>
+    <p>Nombre del alumno: Carlo Valenzuela</p>
+    <span>Carrera: TICS</span>
     <br>
     <span>Moldelo DOM</span>
-    <p>Tecnológias de la Información</p>
+    <p id="parr2">Tecnológias de la Información</p>
+    <script>
+        let parr = document.getElementByTagName('p');
+        console.log(parr.length);
+
+        let parr2 = document.getElementById('parr2');
+        parr2.innerHTML= "DN";
+    </script>    
 
 
 </body>
